10b009b8657c63edfd512cc444129adb5bb9c251
Rye29 <bhudick@ucsc.edu>
2025-04-03 13:28:22 -0700

    glitch sketch working
    transfered glitch components into main website

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -15,20 +15,61 @@ class MyProjectClass {
     this.property1 = param1;
     this.property2 = param2;
   }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
-  }
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  const fillers = {
+    greeting: ["Hello there", "Good day", "こんにちわ", "Hi"],
+    action: ["cooking", "model-kit-building", "painting", "writing"],
+    conflict: ["hard", "expensive", "difficult", "impossible", "time consuming"],
+    celebrity: ["Michael from Michael's", "Pablo Picasso", "Wes Modes", "Jason Derulo"],
+    goodbye: ["Bye now", "じゃあまた", "Hope to see you soon", "Best regards"],
+    
+  };
+  
+  const action_cache = fillers.action[Math.floor((fillers.action.length)*Math.random())]
+  const template = `$greeting!
+  
+  I heard you like `+ action_cache +`, I like `+ action_cache +` too!\n 
+  Unfortunately, it can sometimes feel $conflict. That's why Mike's emporium of Arts is here to help all your\n
+  `+ action_cache +` needs! Trusted by people like $celebrity, we pride ourselves on quality assurance!\n
+  We'll price match the competition everytime!\n
+  \n
+  $goodbye!
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+    console.log("generated");
+  }
+  
+  $("#clicker").click(generate);
 
-  // call a method on the instance
-  myInstance.myMethod();
+  generate();
 }
 
+
+
 // let's get this party started - uncomment me
-//main();
+main();
